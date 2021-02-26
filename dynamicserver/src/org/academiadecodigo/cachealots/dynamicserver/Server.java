b544be22fe9b0be66279a9900d57591c676053ef
Pedro Pacheco de Sousa <codecadet@richie.local>
2021-02-26 17:03:34 -0100

    Adds a single thread server to be upgraded later into multi thread server

--- dynamicserver/src/org/academiadecodigo/cachealots/dynamicserver/Server.java
+++ dynamicserver/src/org/academiadecodigo/cachealots/dynamicserver/Server.java
@@ -1,21 +1,36 @@
 package org.academiadecodigo.cachealots.dynamicserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.ServerSocket;
 
 public class Server {
     public static void main(String[] args) {
 
-        int portNumber = 8080;
+        //int portNumber = 8080;
+        // Need a server to listen to a client
+        try { // TODO: FOLLOW MODULE-2 NETWORK - Sockets
+            // java.net.ServerSocket implements TCP server sockets, used to wait for requests to come in over the network.
+             ServerSocket serverSocket = new ServerSocket(8080); // TCP SERVER SOCKET
 
-        try { //TODO: FOLLOW MODULE-2 NETWORK
-            ServerSocket serverSocket = new ServerSocket(portNumber);
-             Socket clientSocket = serverSocket.accept();
+             // java.net.Socket implements TCP client sockets, capable of connecting to a specified server and port
+             Socket clientSocket = serverSocket.accept(); // Blocking method will wait - TCP CLIENT SOCKET
+
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+             out.println("Welcome to the server.");
+
+             out.close(); // Should everything be closed?
+             in.close();
+             serverSocket.close();
+             clientSocket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
