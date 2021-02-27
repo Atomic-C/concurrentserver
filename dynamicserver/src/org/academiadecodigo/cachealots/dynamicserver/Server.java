bd0198c5bab5c19b7bf77257e8ee12505c06dfa2
Pedro Pacheco de Sousa <codecadet@richie.local>
2021-02-27 20:40:25 -0100

    adds the so awaited multi thread feature to the server. We can now have users each on their very own thread :D

--- dynamicserver/src/org/academiadecodigo/cachealots/dynamicserver/Server.java
+++ dynamicserver/src/org/academiadecodigo/cachealots/dynamicserver/Server.java
@@ -1,9 +1,6 @@
 package org.academiadecodigo.cachealots.dynamicserver;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 
@@ -29,9 +26,8 @@ public class Server {
 
             Socket clientSocket = serverSocket.accept(); // Blocking method will wait - TCP CLIENT SOCKET
             connectionCount++; // Count established connections since server started
-            System.out.println("Player #" + connectionCount + " has joined.");
+            System.out.println("Player #" + connectionCount + " has joined at socket: " + clientSocket);
 
-             // TODO: IMPLEMENT MULTI THREAD
             // Out-puts message to client
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             out.println("Welcome, you are player #" + connectionCount + " today.");
@@ -40,18 +36,58 @@ public class Server {
             // We're not really using input from user...should i keep this here?
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+            // TODO: IMPLEMENT MULTI THREAD
+            // Here we create a new task
+            MyTasker myTask = new MyTasker(clientSocket); // We pass in it's clientSocket
+
+            Thread thread = new Thread(myTask);
+            thread.start();
+
         }
     }
 
     // My tasker will be responsible for the client connections
     public class MyTasker implements  Runnable {
+        // Class properties used to save what we pass in to the constructor
+        private Socket clientSocket;
+        private BufferedReader in;
+        private PrintWriter out;
 
-        public MyTasker(Socket clientSocket) {
+
+        // Class constructor
+        public MyTasker(Socket clientSocket) throws IOException {
+            this.clientSocket = clientSocket; // This is the clientSocket the user connected with
+
+            // TODO: Ask mcs about the difference between the two "out"s
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            //out = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()), true); // What is the difference between these two??
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
 
         }
 
         @Override
         public void run() {
+
+            // access directly Thread class and print it's name
+            System.out.println("This " + Thread.currentThread().getName() + " has started.");
+
+            while (!clientSocket.isClosed()) { // While client socket isn't closed
+
+                try {
+
+                    String line = in.readLine(); // Blocking readLine waiting for new messages
+
+                if (line.equals("quit")) { // if user inserts "quit" string
+                    this.clientSocket.close(); // this. to be sure the current clientSocket is the one closing
+                    System.out.println(Thread.currentThread().getName() + " quit, closing socket.");
+                }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
 
         }
     }
