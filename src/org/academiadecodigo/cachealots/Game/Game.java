fb658e03907573161813d824658615fce1be9f96
jose <jdcp98@gmail.com>
2021-02-28 18:11:01 -0100

    adds  arraylist and prompts for the menu

---
+++ src/org/academiadecodigo/cachealots/Game/Game.java
@@ -0,0 +1,23 @@
+package org.academiadecodigo.cachealots.Game;
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+import org.academiadecodigo.cachealots.dynamicserver.Server;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Game {
+    Server server;
+    Server.MyTasker s;
+
+    Prompt prompt;
+
+    public void P(){
+
+    }
+
+
+
+
+}
--- src/org/academiadecodigo/cachealots/dynamicserver/Server.java
+++ src/org/academiadecodigo/cachealots/dynamicserver/Server.java
@@ -1,23 +1,36 @@
 package org.academiadecodigo.cachealots.dynamicserver;
 
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
 
     ExecutorService pool;
+    Prompt prompt;
+    ArrayList<MyTasker> cona;
 
     public Server(){
         pool = Executors.newFixedThreadPool(4);
     }
 
     public static void main(String[] args) throws IOException {
-
         Server server = new Server(); // Start server instance
+
+
+
         server.startServer(); // Start server through startServer method
+
+
+
+
 
 
 
@@ -26,7 +39,7 @@ public class Server {
     }
 
     public void startServer() throws IOException {
-
+        cona = new ArrayList<>();
         int connectionCount = 0;
         ServerSocket serverSocket = new ServerSocket(8080); // TCP SERVER SOCKET
 
@@ -46,7 +59,9 @@ public class Server {
 
             // TODO: IMPLEMENT MULTI THREAD
             // Here we create a new task
-            MyTasker myTask = new MyTasker(clientSocket); // We pass in it's clientSocket
+            MyTasker myTask = new MyTasker(clientSocket, this); // We pass in it's clientSocket
+            cona.add(myTask);
+
 
             //Thread thread = new Thread(myTask);
             //thread.start();
@@ -55,16 +70,27 @@ public class Server {
     }
 
     // My tasker will be responsible for the client connections
-    public class MyTasker implements  Runnable {
+    public static class MyTasker implements  Runnable {
         // Class properties used to save what we pass in to the constructor
-        private Socket clientSocket;
+        private static Socket clientSocket;
         private BufferedReader in;
         private PrintWriter out;
+        private Prompt prompt;
+        private  Server server;
 
+
+        public static InputStream getIn() throws IOException {
+            return clientSocket.getInputStream();
+        }
+
+        public static OutputStream getOut() throws IOException {
+            return clientSocket.getOutputStream();
+        }
 
         // Class constructor
-        public MyTasker(Socket clientSocket) throws IOException {
+        public MyTasker(Socket clientSocket, Server server) throws IOException {
             this.clientSocket = clientSocket; // This is the clientSocket the user connected with
+            this.server = server;
 
             // TODO: Ask mcs about the difference between the two "out"s
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -72,6 +98,7 @@ public class Server {
             out = new PrintWriter(clientSocket.getOutputStream(), true);
 
         }
+private  String line;
 
         @Override
         public void run() {
@@ -83,7 +110,8 @@ public class Server {
 
                 try {
 
-                    String line = in.readLine(); // Blocking readLine waiting for new messages
+                     line = in.readLine(); // Blocking readLine waiting for new messages
+                    System.out.println(line);
 
                 if (line.equals("quit")) { // if user inserts "quit" string
                     this.clientSocket.close(); // this. to be sure the current clientSocket is the one closing
@@ -92,6 +120,30 @@ public class Server {
 
                 else if (line.equals("RPS")) {
                     out.println("Welcome to Rock Paper Scissors!");
+                    try {
+                        prompt = new Prompt(MyTasker.getIn(),new PrintStream(MyTasker.getOut()));
+                        StringInputScanner question1 = new StringInputScanner();
+                        question1.setMessage("What is your name?");
+                        String name = prompt.getUserInput(question1);
+                        System.out.println(name);
+                        StringInputScanner question2 = new StringInputScanner();
+                        question2.setMessage("Hello " + name);
+                        System.out.println("pick motherfucker");
+                        String[] options = {"Rock", "Paper","Isqueiro"};
+                        MenuInputScanner scanner = new MenuInputScanner(options);
+                        scanner.setMessage("PICK BITCH");
+                        int answerIndex = prompt.getUserInput(scanner);
+
+                        System.out.println("User wants to " + options[answerIndex - 1]);
+                        
+
+
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
 
                 } catch (IOException e) {
@@ -101,6 +153,10 @@ public class Server {
 
             }
 
+        }
+
+        public  String getLine() {
+            return line;
         }
     }
 
